ebd7eda5c57d73787d8ffcabb7f43a842e84e6c5
Shiwayari <shiwayari@gmail.com>
2020-03-24 14:11:37 +0100

    Change to CMake + mk build & move templates to files

--- init_cpp_project.sh
+++ init_cpp_project.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+this_script_dir="$(cd "$(dirname "{BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+
 target_dir=${1:?'Error: no target directory specified.'}
 
 if [ -a "$target_dir" ]; then
@@ -10,48 +12,12 @@ fi
 mkdir ${target_dir}
 cd ${target_dir}
 mkdir src bin include build
-touch includes libs
-echo -n '.project
-.cproject
-.settings/
-bin/
-build/
-includes
-libs' >> .gitignore
-echo -n 'SHELL=/bin/bash
+cp "${this_script_dir}/main_template src/main.cc
+cp "${this_script_dir}/mk_template mk
+cp "${this_script_dir}/CMakeLists_template CMakeLists.txt
+cp "${this_script_dir}/gitignore_template .gitignore
 
-CXX=g++
-CXXFLAGS=-std=c++11
-
-BIN_DIR=bin/
-BUILD_DIR=build/
-SRC_DIR=src/
-INCLUDE_DIR=include/
-
-OBJ_NAMES=
-BIN_NAMES='"$(basename ${target_dir})"'
-
-OBJS=${OBJ_NAMES:%=${BUILD_DIR}/%.o}
-TARGETS=${BIN_NAMES:%=${BIN_DIR}/%}
-LIBS=
-
-all: ${TARGETS}
-
-${TARGETS}: ${BIN_DIR}/%: ${SRC_DIR}/%.cc ${OBJS}
-	readarray deps < <(depfinder.sh . $<);\
-	${CXX} ${CXXFLAGS} -o $@ $^ -I"${INCLUDE_DIR}" "$${deps[@]}" ${LIBS}
-	
-${OBJS}: ${BUILD_DIR}/%.o: ${SRC_DIR}/%.cc  ${INCLUDE_DIR}/%.hh
-	readarray deps < <(depfinder.sh . $<);\
-	${CXX} ${CXXFLAGS} -c -o $@ $< -I"${INCLUDE_DIR}" "$${deps[@]}"
-
-clean:
-	rm -f ${BIN_DIR}/* ${BUILD_DIR}/*' >> Makefile
-
-echo -n 'main (int argc, char** argv) {
-	return 0;
-}' >> "src/$(basename ${target_dir}).cc"
 git init
 git checkout -b devel
 git add .
-git commit -m "First commit, produced by init_cpp_project.sh"
+git commit -m "First commit, created by init_cpp_project.sh"
