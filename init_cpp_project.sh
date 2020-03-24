5a5a9f033e8650315dbc7eb5b9b25b7e50fc2531
Shiwayari <shiwayari@gmail.com>
2020-03-24 14:34:00 +0100

    Replace project_name with target name. Check target for subdirs

--- init_cpp_project.sh
+++ init_cpp_project.sh
@@ -4,6 +4,11 @@ this_script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
 target_dir=${1:?'Error: no target directory specified.'}
 
+echo "${target_dir}" | grep -E '/|\\' && {
+	echo "Error: target directory must be in current dir"
+	exit
+}
+
 if [ -a "$target_dir" ]; then
 	echo "Error: file '${target_dir}' already exists"
 	exit
@@ -14,8 +19,9 @@ cd ${target_dir}
 mkdir src bin include build
 cp "${this_script_dir}/main_template" src/main.cc
 cp "${this_script_dir}/mk_template" mk
-cp "${this_script_dir}/CMakeLists_template" CMakeLists.txt
 cp "${this_script_dir}/gitignore_template" .gitignore
+
+sed 's/project_name/'"${target_dir}"'/g' "${this_script_dir}/CMakeLists_template" > CMakeLists.txt
 
 git init
 git checkout -b devel
