d9b456244486318b1360c22554a1be9300a580b2
Shiwayari <shiwayari@gmail.com>
2020-03-24 14:22:33 +0100

    Fix missing '"'s and '$'

--- init_cpp_project.sh
+++ init_cpp_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-this_script_dir="$(cd "$(dirname "{BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+this_script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
 target_dir=${1:?'Error: no target directory specified.'}
 
@@ -12,10 +12,10 @@ fi
 mkdir ${target_dir}
 cd ${target_dir}
 mkdir src bin include build
-cp "${this_script_dir}/main_template src/main.cc
-cp "${this_script_dir}/mk_template mk
-cp "${this_script_dir}/CMakeLists_template CMakeLists.txt
-cp "${this_script_dir}/gitignore_template .gitignore
+cp "${this_script_dir}/main_template" src/main.cc
+cp "${this_script_dir}/mk_template" mk
+cp "${this_script_dir}/CMakeLists_template" CMakeLists.txt
+cp "${this_script_dir}/gitignore_template" .gitignore
 
 git init
 git checkout -b devel
