050c10fb22fd2eab3776befe132c2ef21a45092a
Shiwayari <shiwayari@gmail.com>
2020-08-17 17:19:53 +0200

    Update error checks. Commit in master

--- init_cpp_project.sh
+++ init_cpp_project.sh
@@ -2,21 +2,26 @@
 
 this_script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
+if ![ -d "${this_script_dir}" ]; then
+	echo "Error: failed to locate install script"
+	exit 1
+fi
+
 target_dir=${1:?'Error: no target directory specified.'}
 
 echo "${target_dir}" | grep -E '/|\\' && {
 	echo "Error: target directory must be in current dir"
-	exit
+	exit 1
 }
 
-if [ -a "$target_dir" ]; then
-	echo "Error: file '${target_dir}' already exists"
-	exit
+if [ -e "$target_dir" ]; then
+	echo "Error: '${target_dir}' already exists"
+	exit 1
 fi
 
 mkdir ${target_dir}
 cd ${target_dir}
-mkdir src bin include build
+mkdir src bin lib include build
 cp "${this_script_dir}/main_template" src/main.cc
 cp "${this_script_dir}/mk_template" mk
 cp "${this_script_dir}/gitignore_template" .gitignore
@@ -24,6 +29,6 @@ cp "${this_script_dir}/gitignore_template" .gitignore
 sed 's/project_name/'"${target_dir}"'/g' "${this_script_dir}/CMakeLists_template" > CMakeLists.txt
 
 git init
-git checkout -b devel
 git add .
 git commit -m "First commit, created by init_cpp_project.sh"
+git checkout -b devel
