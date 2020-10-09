57540f3662b657213bf2e5f3d81de7c9025ebca1
Shiwayari <shiwayari@gmail.com>
2020-10-09 22:58:02 +0200

    Add optional local git user name/email args

--- init_cpp_project.sh
+++ init_cpp_project.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-[[ $# -eq 1 ]] || { echo "Usage: $0 <project_name>"; exit 1; }
+usage="Usage: $0 <project_name> [<git_user_name> <git_user_email>]"
+
+for arg in "$@"; do
+	if [[ "$arg" =~ (-h|--help) ]]; then
+		echo "$usage"
+		exit
+	fi
+done
+
+git_local_user=false
+
+if [[ $# -eq 3 ]]; then
+	git_local_user=true
+	git_user_name="$2"
+	git_user_email="$3"
+	if [[ "$git_user_name" == "" ]] || [[ "$git_user_email" == "" ]]; then
+		echo "Error: empty user name/email"
+		exit 1
+	fi
+elif [[ $# -ne 1 ]]; then
+	echo "$usage"
+	exit 1
+fi
 
 this_script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
@@ -31,6 +53,10 @@ cp "${this_script_dir}/gitignore_template" .gitignore
 sed 's/project_name/'"${target_dir}"'/g' "${this_script_dir}/CMakeLists_template" > CMakeLists.txt
 
 git init
+if $git_local_user; then
+	git config --local --add user.name "$git_user_name"
+	git config --local --add user.email "$git_user_email"
+fi
 git add .
 git commit -m "First commit, created by init_cpp_project.sh"
 git checkout -b devel
