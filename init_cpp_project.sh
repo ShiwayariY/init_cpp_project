48fe6421346fb96d0f8fe42a5064d022c8cb6fd7
Shiwayari <shiwayari@gmail.com>
2022-01-11 19:53:26 +0100

    Refactor options & add local sshkey option

--- init_cpp_project.sh
+++ init_cpp_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage="Usage: $0 <project_name> [<git_user_name> <git_user_email>]"
+usage="Usage: $0 [-h | --help] [-u <git_user_name>] [-e <git_user_email>] [-s <git_ssh_key>] <project_name>"
 
 for arg in "$@"; do
 	if [[ "$arg" =~ (-h|--help) ]]; then
@@ -9,17 +9,26 @@ for arg in "$@"; do
 	fi
 done
 
-git_local_user=false
+git_local_user=""
+git_local_email=""
+git_local_sshkey=""
 
-if [[ $# -eq 3 ]]; then
-	git_local_user=true
-	git_user_name="$2"
-	git_user_email="$3"
-	if [[ "$git_user_name" == "" ]] || [[ "$git_user_email" == "" ]]; then
-		echo "Error: empty user name/email"
-		exit 1
-	fi
-elif [[ $# -ne 1 ]]; then
+while getopts "u:e:s:"; do
+	case "$opt" in
+		u)
+			git_local_user="$OPTARG"
+			;;
+		e)
+			git_local_email="$OPTARG"
+			;;
+		d)
+			git_local_sshkey="$OPTARG"
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+if [[ $# -ne 1 ]]; then
 	echo "$usage"
 	exit 1
 fi
@@ -44,7 +53,10 @@ if [ -e "$target_dir" ]; then
 fi
 
 mkdir ${target_dir}
-cd ${target_dir}
+cd ${target_dir} || {
+	echo "Error: failed to cd into '${target_dir}'"
+	exit 1
+}
 mkdir src bin lib include build
 cp "${this_script_dir}/main_template" src/main.cc
 cp "${this_script_dir}/mk_template" mk
@@ -53,10 +65,9 @@ cp "${this_script_dir}/gitignore_template" .gitignore
 sed 's/project_name/'"${target_dir}"'/g' "${this_script_dir}/CMakeLists_template" > CMakeLists.txt
 
 git init
-if $git_local_user; then
-	git config --local --add user.name "$git_user_name"
-	git config --local --add user.email "$git_user_email"
-fi
+[[ "${git_local_user}" == "" ]] || git config --local --add user.name "${git_local_user}"
+[[ "${git_local_email}" == "" ]] || git config --local --add user.email "${git_local_email}"
+[[ "${git_local_sshkey}" == "" ]] || git config --local core.sshCommand "/usr/bin/ssh/ -i ${git_local_sshkey}"
 git add .
 git commit -m "First commit, created by init_cpp_project.sh"
 git checkout -b devel
